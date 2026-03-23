8a9256c31243b0523e26ee0d5310689318a14888
juli-cloudar <julian.herrmann.2007@gmail.com>
2026-03-23 17:49:23 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export', // ❌ diese Zeile muss entfernt oder auskommentiert werden
-  images: {
-    unoptimized: true,
-  },
-  allowedDevOrigins: ['192.168.2.55'],
+  images: { unoptimized: true },
+  // output: 'export',  ← diese Zeile entfernen
 };
 
 export default nextConfig;
+
