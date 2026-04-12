49950460013aa6d6f762118232ec9a03fc8e2c4d
juli-cloudar <julian.herrmann.2007@gmail.com>
2026-04-12 22:03:02 +0200

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -78,7 +78,7 @@ export default function ManagementPanel() {
       setAuthMode('admin');
       setCurrentUser({ 
         id: 0, 
-        username: 'Admin', 
+        username: 'Julian', 
         role: 'Admin', 
         password: '',
         login_count: 0,
