10cdd08f3a3a772d9f5399b0723a74cdb55d7f26
juli-cloudar <julian.herrmann.2007@gmail.com>
2026-04-12 15:32:08 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export',  // Auskommentiert!
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
