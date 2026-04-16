a999aba062f2d28ecd81eb67ef08b06d1301ef66
juli-cloudar <julian.herrmann.2007@gmail.com>
2026-04-16 23:19:35 +0200

    Update productCleaner.ts

--- src/lib/productCleaner.ts
+++ src/lib/productCleaner.ts
@@ -202,7 +202,7 @@ export function cleanProduct(rawData: any): CleanedProduct {
   }
   
   // Marke korrekt schreiben (ersten Buchstaben groß, rest klein)
-  brand = brand.split(' ').map(word => 
+    brand = brand.split(' ').map((word: string) => 
     word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
   ).join(' ');
   
